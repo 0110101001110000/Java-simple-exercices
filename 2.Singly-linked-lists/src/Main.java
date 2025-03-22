2a2191307e4af3049c65d74bd272fe0c5a871692
01101010-01110000 <0110101001110000_WK@proton.me>
2025-03-22 14:23:59 -0300

    Add professor code.

--- 2.Singly-linked-lists/src/Main.java
+++ 2.Singly-linked-lists/src/Main.java
@@ -1,5 +1,25 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
+
+public class Main
+{
+	public static void main(String[] args) {
+		
+		Aluno a1 = new Aluno("Anderson", 42);
+		Aluno a2 = new Aluno("Ana", 31);
+		Aluno a3 = new Aluno("José", 23);
+
+	    ListaSimples lista = new ListaSimples();
+        
+        lista.adicionaInicio(a1);  //funcionando
+        lista.adicionaInicio(a2);  //funcionando
+        lista.adicionaInicio(a3);  //funcionando        
+
+        lista.removeInicio();  //funcionando
+        
+        a1 = (Aluno) lista.Recupera(0);
+        a2 = (Aluno) lista.Recupera(1);
+
+        System.out.println(a1.getNome() + " - " + a1.getIdade());  //funcionando
+        System.out.println(a2.getNome() + " - " + a2.getIdade());  //funcionando
+   
+	}
 }
