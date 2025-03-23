6183d644c8788ad9bbc26e7a6faa2cf2e155d57a
01101010-01110000 <0110101001110000_WK@proton.me>
2025-03-22 22:40:35 -0300

    Embeleza classe 'Main.java'.

--- 2.Singly-linked-lists/src/Main.java
+++ 2.Singly-linked-lists/src/Main.java
@@ -2,30 +2,43 @@
 public class Main
 {
 	public static void main(String[] args) {
-		
-		Aluno a1 = new Aluno("Carlos Kayky", 20);
+
+        // Welcome message
+        System.out.println("""
+                  .-')               .-') _                                                           .-') _.-. .-')    ('-. _ .-') _                          .-')   .-') _    .-')   \s
+                 ( OO ).            ( OO ) )                                                         ( OO ) \\  ( OO ) _(  OO( (  OO) )                        ( OO ).(  OO) )  ( OO ). \s
+                (_)---\\_) ,-.-'),--./ ,--,' ,----.    ,--.      ,--.   ,--.       ,--.     ,-.-'),--./ ,--,',--. ,--.(,------\\     .'_        ,--.     ,-.-')(_)---\\_/     '._(_)---\\_)\s
+                /    _ |  |  |OO|   \\ |  |\\'  .-./-') |  |.-')   \\  `.'  /        |  |.-') |  |OO|   \\ |  |\\|  .'   / |  .---,`'--..._)       |  |.-') |  |OO/    _ ||'--...__/    _ | \s
+                \\  :` `.  |  |  |    \\|  | |  |_( O- )|  | OO ).-')     /         |  | OO )|  |  |    \\|  | |      /, |  |   |  |  \\  '       |  | OO )|  |  \\  :` `.'--.  .--\\  :` `. \s
+                 '..`''.) |  |(_|  .     |/|  | .--, \\|  |`-' (OO  \\   /          |  |`-' ||  |(_|  .     |/|     ' _(|  '--.|  |   ' |       |  |`-' ||  |(_/'..`''.)  |  |   '..`''.)\s
+                .-._)   \\,|  |_.|  |\\    |(|  | '. (_(|  '---.'|   /  /\\_        (|  '---.,|  |_.|  |\\    | |  .   \\  |  .--'|  |   / :      (|  '---.,|  |_..-._)   \\  |  |  .-._)   \\\s
+                \\       (_|  |  |  | \\   | |  '--'  | |      | `-./  /.__)        |      (_|  |  |  | \\   | |  |\\   \\ |  `---|  '--'  /       |      (_|  |  \\       /  |  |  \\       /\s
+                 `-----'  `--'  `--'  `--'  `------'  `------'   `--'             `------' `--'  `--'  `--' `--' '--' `------`-------'        `------' `--'   `-----'   `--'   `-----' \s
+        """);
+
+
+        // Objects
+
+        Aluno a1 = new Aluno("Carlos Kayky", 20);
 		Aluno a2 = new Aluno("Victor Rafael", 24);
 		Aluno a3 = new Aluno("Luis Gustavo", 19);
         Aluno a4 = new Aluno("Matheus Gabriel", 19);
 
 	    ListaSimples lista = new ListaSimples();
 
+
+        // Some tests
+
         lista.adicionaInicio(a1);
-
         lista.removeInicio();
-
         lista.adiciona(a1, 0);
-
         lista.adicionaFim(a2);
-
         lista.adiciona(a3, 2);
-
         lista.adiciona(a4, 0);
         
         a1 = (Aluno) lista.Recupera(1);
 
         //System.out.println(a1.getNome() + " - " + a1.getIdade());
         lista.mostrarItems();
-   
 	}
 }
