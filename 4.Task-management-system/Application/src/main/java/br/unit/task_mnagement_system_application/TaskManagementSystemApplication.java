a960e9c51fa8ed3425ea1525b3cb2d76943f6de8
01101010-01110000 <0110101001110000_WK@proton.me>
2025-03-23 23:23:40 -0300

    Separa projeto em 'Application'(back) e 'Gui'(front).

---
+++ 4.Task-management-system/Application/src/main/java/br/unit/task_mnagement_system_application/TaskManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package br.unit.task_mnagement_system_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagementSystemApplication.class, args);
+	}
+
+}
---
+++ 4.Task-management-system/Application/src/test/java/br/unit/task_mnagement_system_application/TaskManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package br.unit.task_mnagement_system_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
