19407ad2bcf941109b77b721474fb8e748c83b28
01101010-01110000 <0110101001110000_WK@proton.me>
2025-03-24 10:18:32 -0300

    Conserta bug nas rotas && Transforma tipo do parâmetro 'id' em 'IdDTO'.

--- 4.Task-management-system/Application/src/main/java/br/unit/task_mnagement_system_application/controllers/TaskController.java
+++ 4.Task-management-system/Application/src/main/java/br/unit/task_mnagement_system_application/controllers/TaskController.java
@@ -1,9 +1,10 @@
 
 package br.unit.task_mnagement_system_application.controllers;
 
+import br.unit.task_mnagement_system_application.models.IdDTO;
 import br.unit.task_mnagement_system_application.models.TaskDTO;
+import br.unit.task_mnagement_system_application.services.TaskService;
 import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,47 +16,39 @@ import java.util.List;
 /**
  * @author 01101010-01110000
  */
-@RestController("/api/task")
+@RestController
 public class TaskController {
 
 
     // Attributes
 
-    private final TaskService taskService;
-
-
-    // Controllers
-
-    @Autowired
-    public TaskController(TaskService taskService) {
-        this.taskService = taskService;
-    }
+    private final TaskService taskService = new TaskService();
 
 
     // Main methods
 
-    @PostMapping
+    @PostMapping("/task")
     private boolean createTask(@Valid @ModelAttribute TaskDTO task) {
         return taskService.createTask(task);
     }
 
-    @GetMapping("/{id}")
-    private TaskDTO findTaskById(@PathVariable(value = "id") int id) {
+    @GetMapping("/task/{id}")
+    private TaskDTO findTaskById(@Valid @PathVariable(value = "id") IdDTO id) {
         return taskService.findTaskById(id);
     }
 
-    @GetMapping
+    @GetMapping("/task")
     private List<TaskDTO> findAllTasks() {
         return taskService.findAllTasks();
     }
 
-    @PutMapping("/{id}")
-    private boolean updateTaskById(@PathVariable(value = "id") int id, @Valid @ModelAttribute TaskDTO task) {
+    @PutMapping("/task/{id}")
+    private boolean updateTaskById(@Valid @PathVariable(value = "id") IdDTO id, @Valid @ModelAttribute TaskDTO task) {
         return taskService.updateTaskById(id, task);
     }
 
-    @DeleteMapping("/{id}")
-    private boolean deleteTaskById(@PathVariable(value = "id") int id) {
+    @DeleteMapping("/task/{id}")
+    private boolean deleteTaskById(@Valid @PathVariable(value = "id") IdDTO id) {
         return taskService.deleteTaskById(id);
     }
 }
