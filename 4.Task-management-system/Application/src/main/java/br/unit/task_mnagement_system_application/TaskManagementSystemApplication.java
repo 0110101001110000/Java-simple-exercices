f5160a17db937286bb186f6db1331a21385e4fa1
01101010-01110000 <0110101001110000_WK@proton.me>
2025-03-24 07:29:29 -0300

    Inicializa classe principal.

--- 4.Task-management-system/Application/src/main/java/br/unit/task_mnagement_system_application/TaskManagementSystemApplication.java
+++ 4.Task-management-system/Application/src/main/java/br/unit/task_mnagement_system_application/TaskManagementSystemApplication.java
@@ -1,12 +1,21 @@
+
 package br.unit.task_mnagement_system_application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+// Init -------------------------------------------------------------------- //
+
+
+/**
+ * @author 01101010-01110000
+ */
 @SpringBootApplication
 public class TaskManagementSystemApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(TaskManagementSystemApplication.class, args);
 	}
 
