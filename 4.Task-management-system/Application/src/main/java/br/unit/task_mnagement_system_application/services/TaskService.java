d8f8c2d5dca78b1fd91520046e4fc89949e2e0e8
01101010-01110000 <0110101001110000_WK@proton.me>
2025-03-24 15:56:58 -0300

    Remove requícios de outros códigos.

--- 4.Task-management-system/Application/src/main/java/br/unit/task_mnagement_system_application/services/TaskService.java
+++ 4.Task-management-system/Application/src/main/java/br/unit/task_mnagement_system_application/services/TaskService.java
@@ -1,13 +1,10 @@
 
 package br.unit.task_mnagement_system_application.services;
 
-import br.unit.task_mnagement_system_application.models.IdDTO;
 import br.unit.task_mnagement_system_application.models.TaskDTO;
-import jakarta.validation.Valid;
-import org.springframework.web.bind.annotation.*;
+import br.unit.task_mnagement_system_application.repositories.TaskDAO;
 
 import java.util.ArrayList;
-import java.util.List;
 
 
 // Init -------------------------------------------------------------------- //
@@ -33,31 +30,23 @@ public class TaskService {
 
     // Main methods
 
-    @PostMapping
-    public boolean createTask(@Valid @ModelAttribute TaskDTO task) {
-        if (task.getId() != null) {
-            throw new IllegalArgumentException("O id deve ser nulo.");
-        }
+    public boolean createTask(TaskDTO task) {
         return taskDao.createTask(task);
     }
 
-    @GetMapping("/{id}")
-    public TaskDTO findTaskById(@Valid @PathVariable(value = "id") IdDTO id) {
+    public TaskDTO findTaskById(Integer id) {
         return taskDao.findTaskById(id);
     }
 
-    @GetMapping
-    public List<TaskDTO> findAllTasks() {
+    public ArrayList<TaskDTO> findAllTasks() {
         return taskDao.findAllTasks();
     }
 
-    @PutMapping("/{id}")
-    public boolean updateTaskById(@Valid @PathVariable(value = "id") IdDTO id, @Valid @ModelAttribute TaskDTO task) {
+    public boolean updateTaskById(Integer id, TaskDTO task) {
         return taskDao.updateTaskById(id, task);
     }
 
-    @DeleteMapping("/{id}")
-    public boolean deleteTaskById(@Valid @PathVariable(value = "id") IdDTO id) {
+    public boolean deleteTaskById(Integer id) {
         return taskDao.deleteTaskById(id);
     }
 }
