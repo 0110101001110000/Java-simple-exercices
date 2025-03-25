623e51ab2a6cea3288da7a64178cd3daa8e51098
01101010-01110000 <0110101001110000_WK@proton.me>
2025-03-25 02:32:22 -0300

    Adiciona background, título, e estrutura das tarefas

--- 4.Task-management-system/Gui/gui.py
+++ 4.Task-management-system/Gui/gui.py
@@ -1,4 +1,7 @@
 
+import ttkbootstrap as ttkb
+from PIL import Image, ImageTk
+
 
 # Init ---------------------------------------------------------------------- #
 
@@ -9,8 +12,8 @@ class Gui:
         # Constants
         self.WINDOW_TITLE = "Task Management System"
         self.FONT_NAME    = "Arial"
-        self.X_MIN_SIZE   = 800
-        self.Y_MIN_SIZE   = 600
+        self.width        = 800
+        self.height       = 600
         self.COLOR_1      = "#7a82ff"
         self.COLOR_2      = "#a2b4ff"
 
@@ -18,8 +21,67 @@ class Gui:
         self.root = root
         self.root.config(padx=0, pady=0)
         self.root.title(self.WINDOW_TITLE)
-        self.root.geometry(None)
-        self.root.minsize(self.X_MIN_SIZE, self.Y_MIN_SIZE)
-        self.root.maxsize(self.X_MIN_SIZE, self.Y_MIN_SIZE)
-        self.root.resizable(False, False)
+        self.root.geometry(f"{self.width}x{self.height}")
+        self.root.minsize(self.width, self.height)
+        self.root.maxsize(None, None)
+        self.root.resizable(True, True)
+
+        # Background
+        self.canvas = ttkb.Canvas(self.root)
+        self.canvas.config(width=self.width, height=self.height, bg="purple")
+        self.background_img = Image.open("assets/images/background.png")
+        self.background_img = self.background_img.resize((self.width, self.height), Image.Resampling.LANCZOS)
+        self.background_img = ImageTk.PhotoImage(self.background_img)
+        self.canvas.create_image(0, 0, image=self.background_img, anchor="nw")
+        self.canvas.pack()
+
+        # Main frame
+        self.main_frame = ttkb.Frame(self.canvas, style="TFrame")
+        self.main_frame.place(x=150, y=50, width=self.width - 300, height=self.height - 100)
+        self.main_frame.grid_columnconfigure(0, weight=1)
+
+        # Title
+        self.title_style = ttkb.Style()
+        self.title_style.configure("title_style.TLabel", font=(self.FONT_NAME, 22, "bold"), foreground=self.COLOR_1)
+        self.title_label = ttkb.Label(self.main_frame, text="To Do List", style="title_style.TLabel")
+        self.title_label.grid(column=0, row=0, pady=32)
+
+        # Tasks frame
+        self.tasks_frame = ttkb.Frame(self.main_frame, style="TFrame")
+        self.tasks_frame.grid(column=0, row=1)
+
+        # Button styles
+        self.btn_style = ttkb.Style()
+        self.btn_style.configure("TButton", font=(self.FONT_NAME, 10), borderwidth=0)
+        self.finish_btn_style = ttkb.Style()
+        self.finish_btn_style.configure("finish.TButton")
+        self.finish_btn_style.map("finish.TButton", background=[("!active", "#A4CE95"), ("active", "#7dbc65")])
+        self.edit_btn_style = ttkb.Style()
+        self.edit_btn_style.configure("edit.TButton")
+        self.edit_btn_style.map("edit.TButton", background=[("!active", "#a2b4ff"), ("active", "#7a82ff")])
+        self.delete_btn_style = ttkb.Style()
+        self.delete_btn_style.configure("delete.TButton")
+        self.delete_btn_style.map("delete.TButton", background=[("!active", "#ffa0a0"), ("active", "#ff8484")])
+
+        # Tasks list
+        for i in range(8):
+            # Finish task button
+            self.finish_btn = ttkb.Button(self.tasks_frame, text="✓", style="finish.TButton")
+            self.finish_btn.config(width=1)
+            self.finish_btn.grid(column=0, row=i, padx=5, pady=5, ipadx=1, ipady=1)
+
+            # Task entry
+            self.task_entry = ttkb.Entry(self.tasks_frame, style="task.TEntry", font=(self.FONT_NAME, 10, "bold"))
+            self.task_entry.config(width=40)
+            self.task_entry.grid(column=1, row=i)
+
+            # Task editor button
+            self.edit_btn = ttkb.Button(self.tasks_frame, text="✎", style="edit.TButton")
+            self.edit_btn.config(width=1)
+            self.edit_btn.grid(column=2, row=i, padx=5, pady=5, ipadx=1, ipady=1)
+
+            # Task delete button
+            self.delete_btn = ttkb.Button(self.tasks_frame, text="x", style="delete.TButton")
+            self.delete_btn.config(width=1.25)
+            self.delete_btn.grid(column=3, row=i, padx=0, pady=0, ipadx=1, ipady=1)
 
