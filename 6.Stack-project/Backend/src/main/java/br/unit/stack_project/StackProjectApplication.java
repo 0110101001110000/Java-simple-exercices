bbdc059c1467e714b696ec69bfff5ffbe42df1e2
01101010-01110000 <0110101001110000_WK@proton.me>
2025-04-04 21:38:18 -0300

    Initial commit.

---
+++ 6.Stack-project/Backend/src/main/java/br/unit/stack_project/StackProjectApplication.java
@@ -0,0 +1,13 @@
+package br.unit.stack_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StackProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StackProjectApplication.class, args);
+	}
+
+}
---
+++ 6.Stack-project/Backend/src/test/java/br/unit/stack_project/StackProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package br.unit.stack_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StackProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
