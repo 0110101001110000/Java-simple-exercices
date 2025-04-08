acf2d2fdfa882d24131891383017b7dde65ce4f3
01101010-01110000 <0110101001110000_WK@proton.me>
2025-04-08 08:15:08 -0300

    Comite inicial.

---
+++ 6.Stack-project/Gui/browser.py
@@ -0,0 +1,12 @@
+
+import ttkbootstrap as ttkb
+
+
+# Init ---------------------------------------------------------------------- #
+
+
+class Browser:
+
+    def __init__(self, root: ttkb.Window):
+
+            self.root = root
---
+++ 6.Stack-project/Gui/main.py
@@ -0,0 +1,13 @@
+
+import ttkbootstrap as ttkb
+from browser import Browser
+
+
+# Init ---------------------------------------------------------------------- #
+
+
+if "__name__" == "__main__":
+
+    root    = ttkb.Window()
+    browser = Browser(root)
+    root.mainloop()
