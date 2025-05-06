a05f1420f28b9464cb9c88ae29e1b1cdd746519c
01101010-01110000 <0110101001110000_WK@proton.me>
2025-05-06 10:13:03 -0300

    Implementa testes simples.

--- 7.Hash-table-project/src/main/java/br/unit/Main.java
+++ 7.Hash-table-project/src/main/java/br/unit/Main.java
@@ -13,5 +13,35 @@ public class Main {
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
         }
+
+
+        // Some simple tests
+
+        HashTable hashTable = new HashTable(10);
+
+        User user1 = new User("Pietro Katbush", "asdf");
+        User user2 = new User("Pietra Katbush", "asdf");
+        User user3 = new User("Pietra Katbush", "123456");
+
+        hashTable.insert(user1);
+        hashTable.insert(user2);
+        hashTable.insert(user3);
+
+        System.out.println("\n" + hashTable.search(user1));
+        System.out.println(hashTable.search(user2));
+        System.out.println(hashTable.search(user3));
+
+        System.out.println("\n" + hashTable.delete(user1));
+        System.out.println(hashTable.delete(user2));
+        System.out.println(hashTable.delete(user3));
+
+        System.out.println("\n" + hashTable.search(user1));
+        System.out.println(hashTable.search(user2));
+        System.out.println(hashTable.search(user3));
+
+        System.out.println("\n" + hashTable.isEmpty());
+        System.out.println(hashTable.isFull());
+        System.out.println(hashTable.getLoadFactor());
+
     }
 }
