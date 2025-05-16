17b282749e0e7eae5def9cbbc2318131ad906fe3
victoorfeitosa <168577594+victoorfeitosa@users.noreply.github.com>
2025-05-16 15:59:40 -0300

    Alterações de implementações efetuadas na main e inserção da classe MenuPrompt.

--- 7.Hash-table-project/src/main/java/br/unit/Main.java
+++ 7.Hash-table-project/src/main/java/br/unit/Main.java
@@ -1,5 +1,7 @@
 
 package br.unit;
+import java.util.Scanner;
+import static br.unit.utils.MenuPrompt.exibirMenu;
 
 
 // Init -------------------------------------------------------------------- //
@@ -11,9 +13,45 @@ package br.unit;
  * @since 1.0
  */
 public class Main {
-
     public static void main(String[] args) {
 
-        System.out.println("Hello, World!");
+        HashTable hashTable = new HashTable(10);
+        Scanner scannerEscolha = new Scanner(System.in);
+        int opcao;
+        boolean continuar = true;
+
+        while(continuar) {
+            exibirMenu();
+            System.out.print("Para prosseguir escolha uma das opões acima. \n -------> ");
+            while (!scannerEscolha.hasNextInt()) {
+                System.out.println("Entrada inválida. Por favor, insira um número inteiro.");
+                System.out.print("-------> ");
+                scannerEscolha.next(); // Descarta a entrada inválida
+            }
+            opcao = scannerEscolha.nextInt();
+
+            Scanner scannerUsuario = new Scanner(System.in);
+            switch (opcao) {
+                case 1:
+                    System.out.print("Insira um nome de usuário. \n -------> ");
+                    String nomeUsuario = scannerUsuario.nextLine();
+                    System.out.print("Informe uma senha segura para cadastro. \n -------> ");
+                    String senhaUsuario = scannerUsuario.nextLine();
+                    hashTable.insert(new User(nomeUsuario, senhaUsuario));
+                    System.out.println("Usuário cadastrado com sucesso. ");
+
+                case 2:
+                    System.out.print("Insira o nome do usuário para continuar com a exclusão. \n -------> ");
+                    String deleteNomeUsuario = scannerUsuario.nextLine();
+                    //hashTable.delete();
+
+                case 3:
+                    System.out.print("Insira o nome do usuário para verificar se possui cadastro. \n -------> ");
+                    String buscarUsuario = scannerUsuario.nextLine();
+                    //System.out.println(hashTable.search(buscarUsuario));
+                case 5:
+                    break;
+            }
+        }
     }
 }
