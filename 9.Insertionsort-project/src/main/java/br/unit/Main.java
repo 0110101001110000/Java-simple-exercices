405da01e734002c2bcd6f9d9623ebd0479c5d6a9
01101010-01110000 <0110101001110000_WK@proton.me>
2025-05-19 20:35:34 -0300

    Commit inicial.

---
+++ 9.Insertionsort-project/src/main/java/br/unit/Main.java
@@ -0,0 +1,18 @@
+
+package br.unit;
+
+
+// Init -------------------------------------------------------------------- //
+
+
+/**
+ * ...
+ * @since 1.0
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello, World!");
+    }
+}
