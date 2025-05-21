2da76ea3dfdd07ffb97facd3948a51577f0049bc
Matheus Gabriel <mg2004145@gmail.com>
2025-05-21 16:13:44 -0300

    Importação do pacote java.time.Duration para converter corretamente o tempo do formato MM:SS:MMM para o total de milissegundos.

--- 9.Insertionsort-project/src/main/java/br/unit/Main.java
+++ 9.Insertionsort-project/src/main/java/br/unit/Main.java
@@ -1,6 +1,9 @@
 
 package br.unit;
 
+import br.unit.entities.RaceLapEntity;
+
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.logging.Logger;
 
@@ -38,5 +41,30 @@ public class Main {
         logger.info("# ---------- # Etapa 3: <NOME DA ETAPA AQUI> # ---------- #");
         // <CÓDIGO AQUI>
         logger.info("# ---------- # Fim da etapa 3 # ---------- #");
+
+
+
+        //TESTE
+        RaceLapEntity lap1 = new RaceLapEntity("2025-10-05 10:23:33", "Leonardo Almeida", "Leonardo Almeida", "02:19:718");
+        int millis = TempoDeVolta(lap1);
+        System.out.println("Teste" + lap1.duration() + " = " + millis + " ms");{
+        }
+    }
+
+    //converte corretamente o tempo de volta do formato "MM:SS:MMM" para o total de milissegundos.
+    public static int TempoDeVolta(RaceLapEntity entity){
+        String[] parts = entity.duration().split(":");
+
+        int minutes = Integer.parseInt(parts[0]);
+        int seconds = Integer.parseInt(parts[1]);
+        int miliseconds = Integer.parseInt(parts[2]);
+
+        Duration duration = Duration.ofMinutes(minutes)
+                .plusSeconds(seconds)
+                .plusMillis(miliseconds);
+
+        return (int) duration.toMillis();
+
     }
 }
+
