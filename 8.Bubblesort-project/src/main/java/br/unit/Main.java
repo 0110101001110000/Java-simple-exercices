457699e19b2ba83c2ec8fb89d4c386bc94637de2
victoorfeitosa <168577594+victoorfeitosa@users.noreply.github.com>
2025-05-21 03:17:12 -0300

    feat: implementação da escrita em arquivo .txt

--- 8.Bubblesort-project/src/main/java/br/unit/Main.java
+++ 8.Bubblesort-project/src/main/java/br/unit/Main.java
@@ -3,6 +3,8 @@ package br.unit;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 
@@ -10,7 +12,7 @@ import java.util.Scanner;
 
 
 /**
- * Classe principal para o processo de leitura, ordenação e exibição de informações sobre as empresas.
+ * Classe principal para o processo de leitura, ordenação, exibição de informações sobre as empresas e escrita em novos arquivo txt.
  * @since 1.0
  */
 public class Main {
@@ -69,5 +71,61 @@ public class Main {
                 );
             }
         }
+
+        //--------------
+        String home = System.getProperty("user.dir"); //Busca o caminho que foi alocado o projeto
+        String nomeArquivo = "BubbleSort.txt";
+        String caminho = home + File.separator + nomeArquivo;
+        System.out.println(caminho);
+
+        try{
+            FileWriter Writer = new FileWriter(caminho, false); //parametro false me permite sobrescrever o documento ; enquanto o true adiciona ao documento
+            Writer.write("Lista Ordenada Utilizando BubbleSort \n \n");
+
+            for (Company company : companiesVector) {
+                if (company != null) {
+                    String padraoDados = String.format(
+                            "%s | %s | %.2f \n",
+                            company.name(), company.registrationNumber(), company.marketValue());
+                    Writer.write(padraoDados);
+
+                }
+            }
+            System.out.println("Arquivo foi salvo com sucesso verifique no caminho: \n "+ nomeArquivo);
+            Writer.close();
+        }
+
+        catch (IOException e) {
+            System.err.println("Ocorreu um erro algo gravar o arquivo"+ e.getMessage());
+        }
     }
 }
+
+        /*
+
+        int totalEmpresas = companiesVector.length;
+        int progresso = 100000; // Atualiza a barra a cada 100.000 empresas
+        int processed = 0; // Contador de empresas processadas
+
+
+        processed++; // Incrementa o contador
+        // A cada 100.000 empresas, atualiza a barra de progresso
+        if (processed % progresso == 0) {
+            int percent = (processed * 100) / totalEmpresas;
+            // Monta a barra de progresso
+            StringBuilder barra = new StringBuilder();
+            int barrasCompletas = percent / 10;
+
+            // Preenche com █ (completadas) e ▒ (incompletadas)
+            for (int j = 1; j <= barrasCompletas; j++) {
+                barra.append("█");
+            }
+            for (int j = barrasCompletas + 1; j <= 10; j++) {
+                barra.append("▒");
+            }
+
+            // Limpa a linha anterior e exibe a nova barra
+            System.out.print("\rProcessando: " + barra + " " + percent + "% (" + processed + " empresas)");
+
+        }
+         */
