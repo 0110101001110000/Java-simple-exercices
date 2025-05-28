b637ba2822d53ae2ec7a84684265acd0d3a32881
01101010-01110000 <0110101001110000_WK@proton.me>
2025-05-28 10:49:40 -0300

    Implementa interface gráfica.

--- 10.Selectionsort-project/src/main/java/br/unit/gui/Gui.java
+++ 10.Selectionsort-project/src/main/java/br/unit/gui/Gui.java
@@ -20,71 +20,160 @@ public class Gui extends JFrame {
     // Attributes
 
     private final Logger LOGGER = Logger.getLogger(Gui.class.getName());
-    private final JPanel jPanel = new JPanel();
 
     private String inputFilePath;
     private String outputFilePath;
     private String separator;
 
+    private JTextField inputFilePathField;
+    private JTextField outputFilePathField;
+    private JTextField separatorField;
+    private JButton    submitButton;
+
 
     // Constructors
 
     public Gui() throws HeadlessException {
-        this.setLayout(null);
-        this.setVisible(true);
-        this.setResizable(false);
-        this.setTitle("Java Gui");
-        this.setBackground(Color.WHITE);
+        LOGGER.info("Aplicando configurações iniciais");
+
+        Color backgroundColor = Color.WHITE;
+
+        this.setResizable(true);
+        this.setTitle("Gui-v2");
+        this.setBackground(backgroundColor);
+        this.setLayout(new BorderLayout());
         this.setSize(512, 768);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        jPanel.setLayout(null);
-        jPanel.setBackground(this.getBackground());
-        jPanel.setBounds(0, 0, this.getWidth(), this.getHeight());
 
-        JPanel logoPanel = new JPanel();
-        logoPanel.setBounds(0, 0, jPanel.getWidth(), jPanel.getHeight() / 2);
-        logoPanel.setBackground(jPanel.getBackground());
-        logoPanel.setLayout(new BorderLayout());
+        // Margin panels
 
-        ImageIcon logoPath = new ImageIcon("src/main/java/br/unit/gui/images/c.png");
-        JLabel    logo     = new JLabel(logoPath);
-        logo.setVerticalAlignment(JLabel.CENTER);
-        logo.setHorizontalAlignment(JLabel.CENTER);
-        logoPanel.add(logo);
+        LOGGER.info("Criando painéis de margin");
 
-        JPanel entriesPanel = new JPanel();
-        entriesPanel.setBounds(0, jPanel.getHeight() / 2, jPanel.getWidth(), jPanel.getHeight() / 2);
-        entriesPanel.setBackground(jPanel.getBackground());
-        entriesPanel.setLayout(new BorderLayout());
+        JPanel northJPanel  = new JPanel();
+        JPanel southJPanel  = new JPanel();
+        JPanel westJPanel   = new JPanel();
+        JPanel eastJPanel   = new JPanel();
+        JPanel centerJPanel = new JPanel();
 
-        JPanel entriesList = new JPanel();
-        entriesList.setBackground(jPanel.getBackground());
-        entriesList.setLayout(new BoxLayout(entriesList, BoxLayout.Y_AXIS));
-        entriesPanel.add(entriesList);
+        northJPanel.setBackground(backgroundColor);
+        southJPanel.setBackground(backgroundColor);
+        westJPanel.setBackground(backgroundColor);
+        eastJPanel.setBackground(backgroundColor);
+        centerJPanel.setBackground(backgroundColor);
 
-        JLabel label = new JLabel("Hello, World!");
-        label.setFont(new Font(null, Font.PLAIN, 24));
-        label.setAlignmentX(Component.CENTER_ALIGNMENT);
-        entriesList.add(label);
+        northJPanel.setPreferredSize(new Dimension(77, 100));
+        southJPanel.setPreferredSize(new Dimension(77, 100));
+        westJPanel.setPreferredSize(new Dimension(51, 100));
+        eastJPanel.setPreferredSize(new Dimension(51, 100));
+        centerJPanel.setPreferredSize(new Dimension(100, 100));
 
-        JTextField inputField = new JTextField("Input File Path:");
-        inputField.setMaximumSize(new Dimension(450, 40));
-        entriesList.add(inputField);
 
-        JTextField separatorField = new JTextField("Separator:");
-        separatorField.setAlignmentX(Component.CENTER_ALIGNMENT);
-        separatorField.setMaximumSize(new Dimension(450, 40));
-        entriesList.add(separatorField);
+        // Content panels
 
-        JTextField outputField = new JTextField("Output File Path:");
-        outputField.setAlignmentX(Component.CENTER_ALIGNMENT);
-        outputField.setMaximumSize(new Dimension(450, 40));
-        entriesList.add(outputField);
+        LOGGER.info("Criando painéis de conteúdo");
 
-        jPanel.add(logoPanel);
-        jPanel.add(entriesPanel);
-        this.add(jPanel);
+        centerJPanel.setLayout(new GridLayout(2, 1, 0, 2));
+
+        JPanel logoJPanel    = new JPanel();
+        JPanel entriesJPanel = new JPanel();
+        JPanel entriesItemsJPanel = new JPanel();
+
+        logoJPanel.setBackground(backgroundColor);
+        entriesJPanel.setBackground(backgroundColor);
+        entriesItemsJPanel.setBackground(backgroundColor);
+
+        logoJPanel.setPreferredSize(new Dimension(100, 100));
+        entriesJPanel.setPreferredSize(new Dimension(100, 100));
+        entriesItemsJPanel.setPreferredSize(new Dimension(352, 194));
+
+
+        // Logo content
+
+        LOGGER.info("Adicionando logo");
+
+        logoJPanel.setLayout(new BorderLayout());
+
+        ImageIcon logoIcon = new ImageIcon("src/main/java/br/unit/gui/images/a.png");
+        JLabel    logo     = new JLabel(logoIcon);
+
+        logo.setBackground(backgroundColor);
+        logo.setOpaque(true);
+
+
+        // Form content
+
+        /*LOGGER.info("Adicionando entradas de texto e botão do formulário");
+
+        entriesJPanel.setLayout(new GridLayout(4, 0, 0, 10));
+
+        JPanel inputFieldPanel     = new JPanel(new FlowLayout());
+        JPanel outputFieldPanel    = new JPanel(new FlowLayout());
+        JPanel separatorFieldPanel = new JPanel(new FlowLayout());
+        JPanel submitButtonPanel   = new JPanel(new FlowLayout());
+
+        inputFieldPanel.setBackground(backgroundColor);
+        outputFieldPanel.setBackground(backgroundColor);
+
+        inputFieldPanel.setPreferredSize(new Dimension(100, 100));
+        outputFieldPanel.setPreferredSize(new Dimension(100, 100));
+
+        inputFilePathField = new JTextField();
+        outputFilePathField = new JTextField();
+
+        inputFilePathField.setPreferredSize(new Dimension(350, 38));
+        outputFilePathField.setPreferredSize(new Dimension(350, 38));
+
+        inputFilePathField.setText("Input File Path");
+        outputFilePathField.setText("Output File Path");*/
+
+
+        // Form content
+
+        LOGGER.info("Adicionando entradas de texto e botão do formulário");
+
+        entriesJPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+        entriesItemsJPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 0, 10));
+
+        inputFilePathField  = new JTextField();
+        outputFilePathField = new JTextField();
+        separatorField      = new JTextField();
+        submitButton        = new JButton();
+
+        inputFilePathField.setPreferredSize(new Dimension(350, 38));
+        outputFilePathField.setPreferredSize(new Dimension(350, 38));
+        separatorField.setPreferredSize(new Dimension(350, 38));
+        submitButton.setPreferredSize(new Dimension(350, 38));
+
+        inputFilePathField.setText("Input File Path");
+        outputFilePathField.setText("Output File Path");
+        separatorField.setText("Separator");
+        submitButton.setText("Submit");
+
+
+        // Add all panels
+
+        LOGGER.info("Adicionando painéis à tela");
+
+        logoJPanel.add(logo, BorderLayout.CENTER);
+
+        entriesItemsJPanel.add(inputFilePathField);
+        entriesItemsJPanel.add(outputFilePathField);
+        entriesItemsJPanel.add(separatorField);
+        entriesItemsJPanel.add(submitButton);
+
+        entriesJPanel.add(entriesItemsJPanel);
+
+        centerJPanel.add(logoJPanel);
+        centerJPanel.add(entriesJPanel);
+
+        this.add(northJPanel, BorderLayout.NORTH);
+        this.add(southJPanel, BorderLayout.SOUTH);
+        this.add(westJPanel, BorderLayout.WEST);
+        this.add(eastJPanel, BorderLayout.EAST);
+        this.add(centerJPanel, BorderLayout.CENTER);
+
+        this.setVisible(true);
         this.show();
     }
 
@@ -92,4 +181,9 @@ public class Gui extends JFrame {
     // Methods
 
     // ...
+
+
+    // Classes
+
+
 }
