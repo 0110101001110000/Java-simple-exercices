013af5f6d8953c346bf3e3fd0d6dbb735a89384c
01101010-01110000 <0110101001110000_WK@proton.me>
2025-05-29 01:30:36 -0300

    Commit inicial.

---
+++ 12.Seatwise-project/Backend/src/main/java/br/unit/seatwise_project/SeatWiseProjectApplication.java
@@ -0,0 +1,13 @@
+package br.unit.seatwise_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeatWiseProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeatWiseProjectApplication.class, args);
+	}
+
+}
---
+++ 12.Seatwise-project/Backend/src/test/java/br/unit/seatwise_project/SeatWiseProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package br.unit.seatwise_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeatWiseProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
