95adb374b30bc66088a0c3f541fdc75222850b11
01101010-01110000 <0110101001110000_WK@proton.me>
2025-05-30 22:17:37 -0300

    Implementa classe 'ApiConfig'.

---
+++ 12.Seatwise-project/Gui/src/main/java/br/unit/seatwise_project/config/ApiConfig.java
@@ -0,0 +1,17 @@
+
+package br.unit.seatwise_project.config;
+
+
+// Init -------------------------------------------------------------------- //
+
+
+public class ApiConfig {
+
+
+    // Attributes
+
+    public static final String API_URL          = "http://localhost:8080/";
+    public static final String CLIENT_ENDPOINT  = API_URL + "/client";
+    public static final String CHAIR_ENDPOINT   = API_URL + "/chair";
+    public static final String RESERVE_ENDPOINT = API_URL + "/reserve";
+}
