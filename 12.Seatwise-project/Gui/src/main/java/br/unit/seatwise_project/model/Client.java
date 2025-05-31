71ee15edefa2bc368537e403fb996135a8ef2484
01101010-01110000 <0110101001110000_WK@proton.me>
2025-05-30 21:48:55 -0300

    Implementa alguns POJOs.

---
+++ 12.Seatwise-project/Gui/src/main/java/br/unit/seatwise_project/model/Client.java
@@ -0,0 +1,73 @@
+
+package br.unit.seatwise_project.model;
+
+
+// Init -------------------------------------------------------------------- //
+
+
+public class Client {
+
+
+    // Attributes
+
+    private Long   id;
+    private String email;
+    private String password;
+    private String phone;
+
+
+    // Constructors
+
+    public Client(Long id, String email, String password, String phone) {
+        this.id       = id;
+        this.email    = email;
+        this.password = password;
+        this.phone    = phone;
+    }
+
+
+    // Getter methods
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+
+    // Setter methods
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+
+    // Main methods
+
+    @Override
+    public String toString() {
+        return String.format("%d,%s,%s,%s", this.getId(), this.getEmail(), this.getPassword(), this.getPhone());
+    }
+}
