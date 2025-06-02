6bafc3bed08d9c8d5e88ce0061a4bc1f84cf2937
01101010-01110000 <0110101001110000_WK@proton.me>
2025-06-02 11:58:50 -0300

    Preenche as entidades do 'database' com dados para teste.

---
+++ 12.Seatwise-project/Backend/src/main/java/br/unit/seatwise_project/db/Database.java
@@ -0,0 +1,29 @@
+
+package br.unit.seatwise_project.db;
+
+
+// Init -------------------------------------------------------------------- //
+
+
+public class Database {
+
+
+    // Attributes
+
+    // ...
+
+
+    // Constructors
+
+    // ...
+
+
+    // Getter methods
+
+    // ...
+
+
+    // Main methods
+
+    // ...
+}
